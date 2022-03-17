5afe675568b83c16468afd664f81d81019de28f2
alexlieberman22 <32115248+alexlieberman22@users.noreply.github.com>
2022-03-16 23:12:26 -0600

    Created Toronto neighborhood polygons

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,53 @@
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    Street: streets,
+    Satellite: satelliteStreets,
+    Dark: dark
+};
+
+let map = L.map('mapid', {
+    center: [43.681637, -79.384601],
+    zoom: 10,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+
+let torontoHoods = "https://raw.githubusercontent.com/alexlieberman22/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+
+    L.geoJSON(data, {
+        color: "#0dfba1",
+        weight: 2
+    }).addTo(map);
+});
