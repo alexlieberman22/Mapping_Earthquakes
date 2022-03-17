f92b2a00b2c34ff207875498c38ac003dc063927
alexlieberman22 <32115248+alexlieberman22@users.noreply.github.com>
2022-03-16 22:23:10 -0600

    Add files via upload

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,41 +1,3 @@
-console.log("working");
-
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-];
-
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-L.geoJSON(sanFranAirport, {
-    onEachFeature: function(feature, layer){
-        console.log(feature);
-        layer.bindPopup(`<h2>${feature.properties.city}</h2>`);
-    }
-
-}).addTo(map);
-
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -45,4 +7,34 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: API_KEY
 });
-streets.addTo(map);
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
+
+let airportData = "https://raw.githubusercontent.com/alexlieberman22/Mapping_Earthquakes/main/majorAirports.json";
+
+d3.json(airportData).then(function(data) {
+    console.log(data);
+
+    L.geoJSON(data).addTo(map);
+});
