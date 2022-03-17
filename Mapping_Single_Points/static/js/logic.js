be641ed851cd77d3cf0a64147ce798f89d025f7e
Alex Lieberman <alexlieberman55@hotmail.com>
2022-03-16 20:41:43 -0600

    added multiple population circles to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,6 +1,6 @@
 console.log("working");
 
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,11 +12,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 });
 streets.addTo(map);
 
-let cityData = cities;
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {radius: 300}).addTo(map);
 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {radius: city.population/ 100000})
-    .bindPopup(`<h2>${city.city}, ${city.state}</h2> <hr> <h3>Population ${city.population}</h3>`)
-    .addTo(map)
-});
