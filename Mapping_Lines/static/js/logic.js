65a0db946ef9395daf2f6526bbaaaa958b516a5d
Alex Lieberman <alexlieberman55@hotmail.com>
2022-03-16 20:50:29 -0600

    added lines to map

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,24 @@
+console.log("working");
+
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+streets.addTo(map);
+
+L.polyline(line, {color: "yellow"}).addTo(map);
+
+
