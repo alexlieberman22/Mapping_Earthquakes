70745b56d7d89a049671fa2ed757f6c2a5d17161
Alex Lieberman <alexlieberman55@hotmail.com>
2022-03-16 21:20:14 -0600

    using geoJson

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,6 +1,6 @@
 console.log("working");
 
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 let line = [
     [33.9416, -118.4085],
@@ -9,16 +9,40 @@ let line = [
     [47.4502, -122.3088]
 ];
 
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+L.geoJSON(sanFranAirport, {
+    onEachFeature: function(feature, layer){
+        console.log(feature);
+        layer.bindPopup(`<h2>${feature.properties.city}</h2>`);
+    }
+
+}).addTo(map);
+
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Mapdata &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-streets-v11',
+    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
 });
 streets.addTo(map);
-
-L.polyline(line, {color: "yellow"}).addTo(map);
-
-
